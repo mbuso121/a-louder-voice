3ba04dfe360a207af66645ec2fba23037f1c9fdc
Mbuso Ngobese <106284326+mbuso121@users.noreply.github.com>
2026-08-26 16:22:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 3000,
-  },
   build: {
     outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true,
   },
 })
